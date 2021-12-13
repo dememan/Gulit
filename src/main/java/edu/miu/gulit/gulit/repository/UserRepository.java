8b72dd8072cf491f336f3b318e887d0a6e85e5bb
Kamil0304 <76685553+Kamil0304@users.noreply.github.com>
2021-12-12 20:10:16 -0600

    repo change -backedn only

---
+++ src/main/java/edu/miu/gulit/gulit/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package edu.miu.gulit.gulit.repository;
+
+import edu.miu.gulit.gulit.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository <User, Long> {
+
+    public List<User> findAll();
+    public Optional<User> findById(long id);
+
+    public Optional<User> findByUsername(String username);
+
+    public User save(User user);
+
+    public  void deleteById(long id );
+
+}
