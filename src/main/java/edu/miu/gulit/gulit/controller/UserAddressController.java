4e1c204450ac0d52ecac9cb7bb1e03cdb60d9947
AstewayYiheyis <astewayiheyis@gmail.com>
2021-12-12 23:30:53 -0600

    addressCrud 1

---
+++ src/main/java/edu/miu/gulit/gulit/controller/UserAddressController.java
@@ -0,0 +1,36 @@
+package edu.miu.gulit.gulit.controller;
+
+import edu.miu.gulit.gulit.domain.UserAddress;
+import edu.miu.gulit.gulit.service.UserAddressService;
+import org.apache.tomcat.jni.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Addresses")
+public class UserAddressController {
+    @Autowired
+    UserAddressService userAddressService;
+
+    @GetMapping
+    public List<UserAddress> addresses(){
+        return userAddressService.getAddresses();
+    }
+
+    @PostMapping
+    public UserAddress saveAddress(@RequestBody UserAddress address){
+        return userAddressService.saveAddress(address);
+    }
+
+    @PutMapping("{id}")
+    public UserAddress updateAddress(@RequestBody UserAddress address, @PathVariable("id") int id){
+        return userAddressService.updateAddress(address, id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteAddressById(@PathVariable("id") int id){
+        userAddressService.deleteAddressById(id);
+    }
+}
